9b42076cd65553d7a15351dd3602d2644a089cdb
Joseph Lin <josephlin0922@gmail.com>
2016-09-20 12:53:29 -0400

    Add TestXcodeColorsIOS target

---
+++ TestXcodeColorsIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestXcodeColorsIOS
+//
+//  Created by Joseph Lin on 9/19/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestXcodeColorsIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestXcodeColorsIOS
+//
+//  Created by Joseph Lin on 9/19/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
